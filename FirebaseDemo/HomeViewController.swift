ce75abf3243d2498ee5d564e707d5539cd9e9b78
Jett Anderson <jettanderson@utexas.edu>
2018-02-06 00:21:32 -0600

    Begin Bluetooth Module Work

--- FirebaseDemo/HomeViewController.swift
+++ FirebaseDemo/HomeViewController.swift
@@ -9,11 +9,21 @@
 import Foundation
 import UIKit
 import Firebase
+import CoreBluetooth
 
-class HomeViewController:UIViewController   {
+class HomeViewController:UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate   {
+    
+    var centralManager: CBCentralManager!
+    var timer = Timer()
+    let BLEService_UUID = CBUUID(string: "09fc6363-0292-447a-9757-0210f9a728ed")
+    var peripherals: [CBPeripheral] = []
+    var RSSIs = [NSNumber]()
+    var blePeripheral: CBPeripheral?
+    var data = NSMutableData()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        centralManager = CBCentralManager(delegate: self, queue: nil)
     }
     
     @IBAction func handleLogout(_ target: UIBarButtonItem) {
@@ -21,6 +31,80 @@ class HomeViewController:UIViewController   {
         self.dismiss(animated: false, completion: nil)
         }
     
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == CBManagerState.poweredOn {
+            print("Bluetooth powered on and enabled")
+            startScan()
+        } else {
+            print("Bluetooth disabled, please turn on")
+            let alertVC = UIAlertController(title: "Bluetooth is not enabled", message: "Make sure that your bluetooth is turned on", preferredStyle: UIAlertControllerStyle.alert)
+            let action = UIAlertAction(title: "ok", style: UIAlertActionStyle.default, handler: {(alert: UIAlertAction) -> Void in self.dismiss(animated: true, completion: nil)})
+            alertVC.addAction(action)
+            self.present(alertVC, animated: true, completion: nil)
+        }
+    }
     
+    func startScan() {
+        print("Scanning")
+        self.timer.invalidate()
+        centralManager?.scanForPeripherals(withServices: [BLEService_UUID], options: [CBCentralManagerScanOptionAllowDuplicatesKey:false])
+        Timer.scheduledTimer(timeInterval: 17, target: self, selector: #selector(self.cancelScan), userInfo: nil, repeats: false)
+    }
     
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        //centralManager?.stopScan()
+        self.peripherals.append(peripheral)
+        self.RSSIs.append(RSSI)
+        peripheral.delegate = self
+        peripheral.discoverServices([BLEService_UUID])
+        if blePeripheral == nil {
+            print("We found a new pheripheral devices with services")
+            print("Peripheral name: \(peripheral.name)")
+            print("**********************************")
+            print ("Advertisement Data : \(advertisementData)")
+            blePeripheral = peripheral
+        }
+    }
+    
+    func connectToDevice() {
+        centralManager?.connect(blePeripheral!, options: nil)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print("*****************************")
+        print("Connection complete")
+        print("Peripheral info: \(blePeripheral)")
+        
+        centralManager?.stopScan()
+        print("Scan stopped")
+        data.length = 0
+        
+        peripheral.delegate = self
+        peripheral.discoverServices([BLEService_UUID])
+        
+    }
+    
+    @objc func cancelScan() {
+        self.centralManager?.stopScan()
+        print("Scan Stopped")
+        print("Number of Peripherals Found: \(peripherals.count)")
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        print("*******************************************************")
+        
+        if error != nil {
+            print("Error discovering services: \(error!.localizedDescription)")
+            return
+        }
+        
+        guard let services = peripheral.services else {
+            return
+        }
+        
+        for service in services {
+            peripheral.discoverCharacteristics(nil, for: service)
+        }
+        print("Discovered services: \(services)")
+    }
 }
